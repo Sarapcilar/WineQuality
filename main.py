8038b7cea4fa79918b555d4121dd32de37c9d6e5
Ahmet Enes TURAN <ahmetenesturan@gmail.com>
2022-06-19 13:48:06 +0300

    csv change

--- main.py
+++ main.py
@@ -1,69 +1,51 @@
-from time import sleep
 import numpy as np
 import pandas as pd
-import matplotlib as plt
-#import keras
-
-red = pd.read_csv("data/winedata_red.csv")
-white = pd.read_csv("data/winedata_white.csv")
-
-
-
-
-
-# Add `type` column to `red` with price one
-red['type'] = 1
- 
-# Add `type` column to `white` with price zero
-white['type'] = 0
- 
-# Append `white` to `red`
-wines = red.append(white, ignore_index = True)
- 
-# Import `train_test_split` from `sklearn.model_selection`
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-X = wines.ix[:, 0:11]
-y = np.ravel(wines.type)
- 
-# Splitting the data set for training and validating
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.34, random_state = 45)
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras.layers import * 
+from tensorflow.keras.models import Sequential
+
+dataframe = pd.read_csv("data/winedata_red.csv")
+
+x = dataframe.iloc[:, 0:11]
+y = dataframe.iloc[:, 11]
 
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.34, random_state = 45)
 
-from keras.models import Sequential
- 
-# Import `Dense` from `keras.layers`
-from keras.layers import Dense
- 
-# Initialize the constructor
+#val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
+#train_dataframe = dataframe.drop(val_dataframe.index)
+
+class myCallback(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('acc')>=0.975):
+            self.model.stop_training = True
+callbacks = myCallback()
+
+#creating the model
+
 model = Sequential()
- 
-# Add an input layer
-model.add(Dense(12, activation ='relu', input_shape =(11, )))
- 
-# Add one hidden layer
-model.add(Dense(9, activation ='relu'))
- 
-# Add an output layer
+model.add(Dense(11, activation ='relu', input_shape =(11, )))
+model.add(Dense(24, activation ='relu'))
+model.add(Dense(48, activation ='relu'))
+model.add(Dense(12, activation ='relu'))
 model.add(Dense(1, activation ='sigmoid'))
- 
-# Model output shape
-model.output_shape
- 
-# Model summary
-model.summary()
- 
-# Model config
-model.get_config()
- 
-# List all weight tensors
-model.get_weights()
-model.compile(loss ='binary_crossentropy', optimizer ='adam', metrics =['accuracy'])
 
-# Training Model
-model.fit(X_train, y_train, epochs = 3,
-           batch_size = 1, verbose = 1)
-  
-# Predicting the Value
-y_pred = model.predict(X_test)
-print(y_pred)
+
+print(model.summary())
+
+print(model.get_config())
+ 
+print(model.get_weights())
+model.compile(loss ='binary_crossentropy', optimizer ='adam', metrics =['acc'])
+
+r = model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = 30, callbacks=[callbacks],
+           batch_size = 4, verbose = 1)
+
+
+plt.plot(r.history['acc'], label='accuracy')
+plt.plot(r.history['val_acc'], label='val_accuracy')
+plt.legend()
+plt.savefig('graph.png')
+plt.show()
