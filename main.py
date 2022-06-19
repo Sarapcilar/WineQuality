96332426898e2cef50457b3bf0a8ce8a83de30f4
Ahmet Enes TURAN <ahmetenesturan@gmail.com>
2022-06-19 20:40:34 +0300

    final model

--- main.py
+++ main.py
@@ -1,51 +1,66 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import Callback
-from tensorflow.keras.layers import * 
+from tensorflow.keras.callbacks import Callback 
+from tensorflow.keras.layers import *
 from tensorflow.keras.models import Sequential
+import matplotlib.pyplot as plt
 
-dataframe = pd.read_csv("data/winedata_red.csv")
-
-x = dataframe.iloc[:, 0:11]
-y = dataframe.iloc[:, 11]
+red = pd.read_csv("data/winedata_red.csv", sep=',')
+white = pd.read_csv("data/winedata_white.csv", sep=',')
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.34, random_state = 45)
+red['type'] = 1
+white['type'] = 0
+wines = red.append(white, ignore_index = True)
+wines.head()
+X = wines.drop("quality", axis=1)
+y = np.ravel(wines.quality)
 
-#val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
-#train_dataframe = dataframe.drop(val_dataframe.index)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.34, random_state = 45)
 
 class myCallback(Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if(logs.get('acc')>=0.975):
+        if(logs.get('mse')<0.01):
             self.model.stop_training = True
 callbacks = myCallback()
 
-#creating the model
+from keras.models import Sequential
+ 
+
+from keras.layers import Dense
 
 model = Sequential()
-model.add(Dense(11, activation ='relu', input_shape =(11, )))
+ 
+# Add an input layer
+model.add(Dense(11, activation ='relu', input_shape =(12, )))
+ 
+# Add one hidden layer
 model.add(Dense(24, activation ='relu'))
 model.add(Dense(48, activation ='relu'))
-model.add(Dense(12, activation ='relu'))
-model.add(Dense(1, activation ='sigmoid'))
-
+model.add(Dense(96, activation ='relu'))
+model.add(Dense(256, activation ='relu'))
+model.add(Dense(128, activation ='relu'))
+model.add(Dense(64, activation ='relu'))
+# Add an output layer
+model.add(Dense(1))
+ 
 
 print(model.summary())
-
 print(model.get_config())
- 
 print(model.get_weights())
-model.compile(loss ='binary_crossentropy', optimizer ='adam', metrics =['acc'])
-
-r = model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = 30, callbacks=[callbacks],
-           batch_size = 4, verbose = 1)
+model.compile(loss ='mse', optimizer ='adam', metrics =['mse'])
 
 
-plt.plot(r.history['acc'], label='accuracy')
-plt.plot(r.history['val_acc'], label='val_accuracy')
+r = model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 400, callbacks=[callbacks])
+
+
+plt.plot(r.history['mse'], label='mse')
+plt.plot(r.history['val_mse'], label='val_mse')
 plt.legend()
-plt.savefig('graph.png')
+plt.savefig('figures/graph.png')
 plt.show()
+
+
+y_pred = model.predict(X_test)
+print((y_pred.round()))
